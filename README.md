33ae78e636c1fa7749ab979f25ccd11f6739afe3
Morgan Newman <mpnewman95@gmail.com>
2018-09-06 08:24:47 -0700

    Added v2 files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Node shopping list v1
+Shopping list v2
+================
 
-
-[GitHub](https://github.com/Thinkful-Ed/node-shopping-list-v1) | [Glitch](https://glitch.com/edit/#!/node-shopping-list-v1) 
+https://github.com/Thinkful-Ed/node-shopping-list-v2
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 
 const express = require('express');
-// we'll use morgan to log the HTTP layer
+const router = express.Router();
 const morgan = require('morgan');
-// we'll use body-parser's json() method to 
-// parse JSON data sent in requests to this app
 const bodyParser = require('body-parser');
 
-// we import the ShoppingList model, which we'll
-// interact with in our GET endpoint
 const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
@@ -17,28 +13,44 @@ const app = express();
 app.use(morgan('common'));
 
 // we're going to add some items to ShoppingList
-// so there's some data to look at. Note that 
-// normally you wouldn't do this. Usually your
-// server will simply expose the state of the
-// underlying database.
+// so there's some data to look at
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+// adding some recipes to `Recipes` so there's something
+// to retrieve.
+Recipes.create(
+  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
+Recipes.create(
+  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
 
-Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
-Recipes.create('chocolate truffles', ['cocoa', 'sugar', 'butter']);
-Recipes.create('cake', ['milk', 'flour', 'sugar']);
-
-// when the root of this route is called with GET, return
-// all current ShoppingList items by calling `ShoppingList.get()`
+// when the root of this router is called with GET, return
+// all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
-app.get('/recipes-list', (req, res) => {
-  res.json(Recipes.get());
+app.post('/shopping-list', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'budget'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
 });
+
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
